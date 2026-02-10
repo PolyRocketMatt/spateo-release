b59f0387f3db3855b2ae3d540f8125b60800b05a
YifanLu2000 <lyf048@whu.edu.cn>
2026-02-10 10:47:40 -0800

    Refactor import statements in data_io.py for improved readability and organization.

--- spateo/data_io.py
+++ spateo/data_io.py
@@ -9,6 +9,24 @@ except ImportError:
 
 try:
     # anndata < 0.12 export these at top-level
-    from anndata import read_csv, read_excel, read_hdf, read_loom, read_mtx, read_text, read_umi_tools, read_zarr
+    from anndata import (
+        read_csv,
+        read_excel,
+        read_hdf,
+        read_loom,
+        read_mtx,
+        read_text,
+        read_umi_tools,
+        read_zarr,
+    )
 except ImportError:
-    from anndata.io import read_csv, read_excel, read_hdf, read_loom, read_mtx, read_text, read_umi_tools, read_zarr
+    from anndata.io import (
+        read_csv,
+        read_excel,
+        read_hdf,
+        read_loom,
+        read_mtx,
+        read_text,
+        read_umi_tools,
+        read_zarr,
+    )
